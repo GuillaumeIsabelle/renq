a8dca5f9963ee55e908bd97cf4510ed1988399e5
Jean Guillaume <jgi@jgwill.com>
2019-11-20 13:55:46 -0500

    v0.0.3 - Refactored and fixed issue

--- renq.js
+++ renq.js
@@ -4,49 +4,87 @@
 //ex.   0060-0100 will became 0001-0040
 //
 
+
+//@STCGoal TODO Get from ENV
+var defaultRenamer = "git mv";
+var defaultExt = "jpg";
+
+
+var debug = 0;
+
 var pad = require('pad-number');
 
-var args = process.argv.slice(1);
+var args = process.argv.slice(2);
 
-console.log(` -- RENQ Guillaume D.Isabelle Sequence Renamer, v0.0.2`);
+var p = require('./package.json');
+
+console.log(` -- RENQ Guillaume D.Isabelle Sequence Renamer,
+\tversion : ${p.version}`);
+if (debug > 0)
+{
+
+    console.log(`DEBUG:: 0: ${process.args[0]}`);
+    console.log(`DEBUG:: 1: ${process.args[1]}`);
+    console.log(`DEBUG:: 2: ${process.args[2]}`);
+}
+
 
 if (args[0] == "--help" || args[0] == "--h" || args[0] == "-h" )
 {
-    console.log(`
-    ===SYNOPSIS===
-    ${process.argv.slice(0)[0]} [startNo] [ext] ([mv cmd])
-    ex.
-    ${process.argv.slice(0)[0]} 60 jpg
-    ${process.argv.slice(0)[0]} 60 jpg "git mv"
-    
-    `);
+   showHelp();
 }
 else{
     runProgram();
 }
 
-
 function runProgram()
 {
     
-    var start = 60;
-    var end = 89;
-    var renamer =  args[2]? args[2] : "git mv";
-    var spad = 4; //padding
-    var ext = args[1]? args[1] : "jpg";
-    var F = 4; //0000
+    var start = args[0]? args[0] : -1;
+    var end =args[1]? args[1]  : -1; 
+    // if (end != -1 ) end += 1;
+    var ext = args[2]? args[2] : defaultExt;
+    var spad = args[3]? args[3] :4; //padding
+    var renamer =  args[4]? args[4] : defaultRenamer;
+  
     
-    
-    var c = 1;
-    for (let i =  x = start; i < end; i++) {
-        // console.log(pad(10, 4)); // '0010'
-        var ofn = pad(i,spad) + `.${ext}`;
-        var nfn = pad(c,spad) + `.${ext}`;
-        
-        console.log(`${renamer} ${ofn} ${nfn}`)
-        
-        
-        c++;
-        
+    if (start == -1 || end == -1)
+    {
+console.warn("\nQUITTING, must specify a starting/ending number for the offsetted sequence.");
+showHelp();
     }
+    else {
+
+        var c = 1;
+        for (let i =  x = start; i <= end; i++) {
+            // console.log(pad(10, 4)); // '0010'
+            var ofn = pad(i,spad) + `.${ext}`;
+            var nfn = pad(c,spad) + `.${ext}`;
+            
+            console.log(`${renamer} ${ofn} ${nfn}`)
+            
+            
+            c++;
+            
+        }
+    }
+}
+
+
+
+
+function showHelp()
+{
+    console.log(`
+    ===SYNOPSIS===
+    ${process.argv.slice(0)[0]} [startNo] [endNo] [ext] ([pad] [mv cmd])
+    ex.
+    ${process.argv.slice(0)[0]} 60 89 jpg
+    ${process.argv.slice(0)[0]} 60 89 jpg 4
+    ${process.argv.slice(0)[0]} 60 89 jpg 4 "git mv"
+    ${process.argv.slice(0)[0]} 60 89 jpg 4 "mv"
+    
+
+    
+    `);
 }
