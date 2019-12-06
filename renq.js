ba72af0a442f3fd43c1873d24339cb9f29d0fbdb
Jean Guillaume <jgi@jgwill.com>
2019-12-06 16:03:58 -0500

    fix: int convertion + added debug

--- renq.js
+++ renq.js
@@ -16,16 +16,28 @@ var pad = require('pad-number');
 
 var args = process.argv.slice(2);
 
+// if (process.argv[0] == )
 var p = require('./package.json');
 
 console.log(`// -- RENQ Guillaume D.Isabelle Sequence Renamer,
 //\tversion : ${p.version}`);
 if (debug > 0)
 {
+    console.log("-----------ARGS>>---------------");
+    console.log(args);
+console.log("-----------<<ARGS---------------");
 
+try {
+    console.log(process);
     console.log(`DEBUG:: 0: ${process.args[0]}`);
     console.log(`DEBUG:: 1: ${process.args[1]}`);
     console.log(`DEBUG:: 2: ${process.args[2]}`);
+    
+console.log("---------------------------");
+console.log("---------------------------");
+} catch (error) {
+    
+}
 }
 
 
@@ -39,14 +51,26 @@ else{
 
 function runProgram()
 {
-    
-    var start = args[0]? args[0] : -1;
-    var end =args[1]? args[1]  : -1; 
+    if (debug>0 ) console.log(args);
+    var start = -1;
+    var end = -1;
+
+     try { start = args[0]? args[0] : -1;} catch (error) {}
+     try {  end =args[1]? args[1]  : -1; } catch (error) {}
+
     // if (end != -1 ) end += 1;
-    var ext = args[2]? args[2] : defaultExt;
-    var spad = args[3]? args[3] :4; //padding
-    var renamer =  args[4]? args[4] : defaultRenamer;
-  
+    var ext = defaultExt;
+    var spad = 4;
+    var renamer =defaultRenamer;
+
+    try { ext = args[2]? args[2] : defaultExt;} catch (error) {}
+    try {   spad = args[3]? args[3] :4; } catch (error) {}
+    
+    try { renamer = args[4] ? args[4] : defaultRenamer; } catch (error) { }
+    
+     
+    
+    if (debug>0) { console.log(`start:${start} - end: ${end} - ext: ${ext} - spad: ${spad} - renamer: ${renamer}`);}
     
     if (start == -1 || end == -1)
     {
@@ -54,10 +78,16 @@ console.warn("\nQUITTING, must specify a starting/ending number for the offsette
 showHelp();
     }
     else {
-
         var c = 1;
-        for (let i =  x = start; i <= end; i++) {
-            // console.log(pad(10, 4)); // '0010'
+        var start2 =  Number(start) ;
+        var end2 =  Number(end);
+
+        console.log(`start:${start2} - end: ${end2} - ext: ${ext} - spad: ${spad} - renamer: ${renamer}`);
+
+        for (let i = start2; i <= end2 ; i++) {
+            
+            try {
+                  // console.log(pad(10, 4)); // '0010'
             var ofn = pad(i,spad) + `.${ext}`;
             var nfn = pad(c,spad) + `.${ext}`;
             
@@ -65,6 +95,10 @@ showHelp();
             
             
             c++;
+            } catch (error) {
+                console.warn(error);
+            }
+          
             
         }
     }
